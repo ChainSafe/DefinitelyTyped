e16c98e6af9e4d01885585e92070667b634a2b15
Marin Petrunić <marin.petrunic@gmail.com>
2019-11-15 11:06:37 +0100

    basic bcrypto template

---
+++ types/bcrypto/index.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for bcrypto 4.2
+// Project: https://github.com/bcoin-org/bcrypto
+// Definitions by: Chainsafe <https://github.com/chainsafe>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+export const version: string;
